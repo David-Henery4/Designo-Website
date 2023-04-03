74930f8ee28d44f2846fa7cb4840cc68e06ba565
DizzlyD <david4henery00@hotmail.com>
2023-04-03 21:00:42 +0100

    bg-path-refactor-tailwind-config

--- vite.config.js
+++ vite.config.js
@@ -1,52 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import svgr from "vite-plugin-svgr";
-import copy from "rollup-plugin-copy";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
 
-function copyPlugin() {
-  return copy({
-    targets: [
-      {
-        src: "./src/assets/home/mobile/image-web-design.jpg",
-        dest: "./dist/assets/home/mobile",
-      },
-      {
-        src: "./src/assets/home/tablet/image-web-design.jpg",
-        dest: "./dist/assets/home/tablet",
-      },
-      {
-        src: "./src/assets/home/desktop/image-web-design-large.jpg",
-        dest: "./dist/assets/home/desktop",
-      },
-      {
-        src: "./src/assets/home/mobile/image-app-design.jpg",
-        dest: "./dist/assets/home/mobile",
-      },
-      {
-        src: "./src/assets/home/tablet/image-app-design.jpg",
-        dest: "./dist/assets/home/tablet",
-      },
-      {
-        src: "./src/assets/home/desktop/image-app-design.jpg",
-        dest: "./dist/assets/home/desktop",
-      },
-      {
-        src: "./src/assets/home/mobile/image-graphic-design.jpg",
-        dest: "./dist/assets/home/mobile",
-      },
-      {
-        src: "./src/assets/home/tablet/image-graphic-design.jpg",
-        dest: "./dist/assets/home/tablet",
-      },
-      {
-        src: "./src/assets/home/desktop/image-graphic-design.jpg",
-        dest: "./dist/assets/home/desktop",
-      },
-    ],
-  });
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svgr(), react(), copyPlugin()],
-});
+  plugins: [svgr(), react()],
+})

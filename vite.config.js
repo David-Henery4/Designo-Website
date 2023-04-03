9266d3326b575a09c655475c537a92df7194a55d
DizzlyD <david4henery00@hotmail.com>
2023-04-03 18:17:38 +0100

    background-paths-refactor

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
-
 import copy from "rollup-plugin-copy";
 
 function copyPlugin() {
